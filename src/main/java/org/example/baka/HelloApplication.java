604b5a28f1eafe6e5c4c69cc9c27cb813e8e6a5c
6291758 <douaabhl7@gmail.com>
2025-11-06 14:05:22 -0500

    repond trc

--- src/main/java/org/example/baka/HelloApplication.java
+++ src/main/java/org/example/baka/HelloApplication.java
@@ -16,5 +16,6 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         //BAAAAAKADEEEEEEEE
+        // salut ma gooo sans viceeeee
     }
 }
